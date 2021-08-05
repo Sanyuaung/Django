28d2f1eef3ac6c3e8385915d94ab0ad92116e961
San Yu Aung <sanyuaung.ygn.mm@gmail.com>
2021-08-05 21:31:05 +0630

    lesson 2

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def about(request):
+    return HttpResponse('About Page')
+
+def contact(request):
+    return HttpResponse('contact Page')
+
+def dashboard(request):
+    return HttpResponse('dashboard Page')
 
 # Create your views here.
