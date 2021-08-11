e8d64a90824e8d14535b5c7198a42f9008342129
San Yu Aung <sanyuaung.ygn.mm@gmail.com>
2021-08-11 16:00:18 +0630

    lesson 10

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views;
 
+
 urlpatterns = [
-    path('', views.dashboard),
-    path('customers/<int:id>',views.customers),
-    path('products/',views.products),
+    path('',views.dashboard,name='dashboard'),
+    path('customers/<int:id>',views.customers,name='customers.show'),
+    path('products/',views.products,name='products'),
 ]
