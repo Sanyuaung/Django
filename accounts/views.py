6b1c3b35418b3958293a36408663543d64350bad
San Yu Aung <sanyuaung.ygn.mm@gmail.com>
2021-08-21 12:04:18 +0630

    lesson 13

--- accounts/views.py
+++ accounts/views.py
@@ -56,4 +56,14 @@ def orderUpdate(request,orderId):
     return render(request,'accounts/order_form.html',{
         'form':form
     })
+    
+def orderDelete(request,orderId):
+    order=Order.objects.get(id=orderId);
+    if request.method=="POST":
+        order.delete();
+        return redirect('/');
+                
+    return render(request,'accounts/order_delete.html',{
+        'order':order
+    })
 # Create your views here.
