b8da48647cbb7f4b7c4e22dbd538b4e550657982
San Yu Aung <sanyuaung.ygn.mm@gmail.com>
2021-08-31 20:30:02 +0630

    lesson 29

--- accounts/signals.py
+++ accounts/signals.py
@@ -6,6 +6,6 @@ def create_customer_profile(sender,instance,created,**kwargs):
     if created :
         gp=Group.objects.get(name="Customer")
         instance.groups.add(gp)
-        Customer.objects.create(user=instance)
+        Customer.objects.create(user=instance,email=instance.email)
 
 post_save.connect(create_customer_profile,sender=User)
